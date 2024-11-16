54e92d7d0271fe46f39ed82d9348f02c6a3af23d
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2024-11-16 15:16:30 -0500

    Update finch examples
    Sourced from: https://learn.birdbraintechnologies.com/install-shortcuts/

--- FinchTest.py
+++ FinchTest.py
@@ -1,12 +1,12 @@
 from BirdBrain import Finch
 import time
 
-bird = Finch('A')
+myFinch = Finch('A')
 
 for i in range(0,10):
-    bird.setBeak(100, 100, 100)
+    myFinch.setBeak(100, 100, 100)
     time.sleep(1)
-    bird.setBeak(0, 0, 0)
+    myFinch.setBeak(0, 0, 0)
     time.sleep(1)
 	
 myFinch.stopAll()
